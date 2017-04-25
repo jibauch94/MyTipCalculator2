a84c86045080f50c4c88eacae931ece3e75ba2c0
jibauch94 <jib090994@gmail.com>
2017-04-25 14:45:28 +0200

    2 commit

--- app/src/main/java/com/example/jibba_000/mytipcalculator/MainActivity.java
+++ app/src/main/java/com/example/jibba_000/mytipcalculator/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.jibba_000.mytipcalculator;
 
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.icu.text.NumberFormat.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    // currency and percent formatter objects
+    private static final NumberFormat currencyFormat = getCurrencyInstance();
+
+    private static final NumberFormat percentFormat = getPercentInstance();
+
+
+    private double billAmount = 0.0; //amount edited by the user
+    private double percent = .15; // percent of tips
+    private TextView percentView; // shows tip percent
+    private TextView amountView; // shows the amount without tip
+    private TextView totalLabelView; // shows total amount
+    private TextView tipLabelView; // shows the calculated tip amount
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
